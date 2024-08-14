1cf28befde070ff96d8bbc0d0d0a40b814fe0565
OS-Samir <samiradhikary972@gmail.comr>
2024-08-14 11:17:27 +0545

    async await

--- script.js
+++ script.js
@@ -62,3 +62,9 @@
 
 // console.log(abcd())
 
+async function abcd() {
+  var blob =  await fetch(`https://randomuser.me/api/`)
+  var ans = await blob.json()
+  console.log(ans.results[0].name)
+} 
+abcd();
