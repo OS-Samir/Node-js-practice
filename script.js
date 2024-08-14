dac0d9307da08ec3210ef53fbec0b695c93749cb
OS-Samir <samiradhikary972@gmail.comr>
2024-08-14 10:32:27 +0545

    ma

--- script.js
+++ script.js
@@ -4,7 +4,15 @@
 // async js coding
 //forEach, map, filter, find, indexOf
 
-var arr = [1, 2, 3, 4];
-arr.forEach(function(val){
-   console.log( val + " hello" );
+// var arr = [1, 2, 3, 4]
+
+// arr.forEach(function (val){
+//    console.log(val + " num")
+// })
+
+
+var arr = [1, 2, 3 ,4]
+
+arr.map(function(val){
+   console.log(val + 2);
 })
