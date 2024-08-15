f48161e45d4bf82cdc95c165e27f6e2edac025be
OS-Samir <samiradhikary972@gmail.comr>
2024-08-15 21:47:31 +0545

    writeFile

--- script2.js
+++ script2.js
@@ -6,3 +6,14 @@
 // understanding HTTP module
 // Node.js is js runtime environment
 
+//write file
+//copy file
+//rename
+//appendfile
+//unlink
+
+const fs = require('node:fs');
+fs.writeFile("samir.txt", "hello samir how are you?", function(err){
+    if(err) console.error(err);
+    else console.log("done!");
+})
