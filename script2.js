831663a030a589bf4e6c357f361578e1f227fd5e
OS-Samir <samiradhikary972@gmail.comr>
2024-08-15 21:50:57 +0545

    appendFIle

--- script2.js
+++ script2.js
@@ -12,8 +12,21 @@
 //appendfile
 //unlink
 
+
+// //write File
+// const fs = require('node:fs');
+// fs.writeFile("samir.txt", "hello samir how are you?", function(err){
+//     if(err) console.error(err);
+//     else console.log("done!");
+// })
+
+
+//appendFile
+
 const fs = require('node:fs');
-fs.writeFile("samir.txt", "hello samir how are you?", function(err){
-    if(err) console.error(err);
+fs.appendFile("samir.txt", "ma thik chu dost", function(err) {
+    if (err) {
+        throw err;
+    }
     else console.log("done!");
 })
