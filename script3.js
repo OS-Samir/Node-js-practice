dc2684e655d7f3c85a2e3776b3ecc6fc9f70409d
OS-Samir <samiradhikary972@gmail.comr>
2024-08-16 16:38:35 +0545

    nodemon

--- script3.js
+++ script3.js
@@ -4,3 +4,4 @@
  // dependencies = packages and packages ko dependencies
  // devdependencies = packages that only necessary for development, when the app development is completed and not deployed these dependencies are not used.
  // scripts - understading default scripts PATH and custom scripts
+ // npm i nodemon --save-dev(way to install packages as devdependencies )
