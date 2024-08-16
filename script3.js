280e66659b0cf7914f0e083ebd62b8336d52c4de
OS-Samir <samiradhikary972@gmail.comr>
2024-08-16 16:49:56 +0545

    scripts

--- script3.js
+++ script3.js
@@ -5,3 +5,5 @@
  // devdependencies = packages that only necessary for development, when the app development is completed and not deployed these dependencies are not used.
  // scripts - understading default scripts PATH and custom scripts
  // npm i nodemon --save-dev(way to install packages as devdependencies )
+
+ console.log("hello world");
