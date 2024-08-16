f3d99a800af0957b6adc4fbca4446da83249ee72
OS-Samir <samiradhikary972@gmail.comr>
2024-08-16 22:20:52 +0545

    express

--- script4.js
+++ script4.js
@@ -1,5 +1,6 @@
 // Express.js Framework
-// Introduction to Express.js
+// Introduction to Express.js // Express js is npm package. It is a framework
+// Express manages everything from receiving the request and giving the response
 // Setting up a basic expression application
 // Routing
 // Middleware
