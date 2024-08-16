c5101d397c3d3577b7435623190573dfad3168e7
OS-Samir <samiradhikary972@gmail.comr>
2024-08-16 16:14:10 +0545

    day3 of learning

---
+++ script3.js
@@ -0,0 +1,6 @@
+// NPM understanding
+// installing and uninstalling anything basics and advanced
+ // understanding node_modules
+ // dependencies
+ // devdependencies
+ // scripts - understading default scripts PATH and custom scripts
