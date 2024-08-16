37f7e2e9e0c3fc1027242f7a37ae0c603c0eca7e
OS-Samir <samiradhikary972@gmail.comr>
2024-08-16 22:47:06 +0545

    express

--- script4.js
+++ script4.js
@@ -6,3 +6,16 @@
 // Middleware
 // Request and response handling
 // Error handling
+
+
+const express = require('express');
+const app = express();
+
+app.get("/", function(req, res) {
+    res.send("my name is samir")
+} )
+
+app.get("/profile", function(req, res) {
+    res.send("samir is a champion")
+})
+app.listen(3000);
