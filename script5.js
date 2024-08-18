e2c396b2ab577ae4832c3339373017e3e97d3533
OS-Samir <samiradhikary972@gmail.comr>
2024-08-18 16:48:13 +0545

    day 5 of learning

--- script5.js
+++ script5.js
@@ -1,1 +1,7 @@
 //form handling and working with forms 
+// process: 1)Initialize the project with npm (npm init -y)
+// Install express
+
+// dynamic routing
+// how to get data coming from frontend at backend route
+// setting up parsers for form
