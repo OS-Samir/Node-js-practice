6f7fcf33dc03df7f36b1982c7306fa74b839833c
OS-Samir <samiradhikary972@gmail.comr>
2024-08-18 17:11:26 +0545

    applying public method

--- script5.js
+++ script5.js
@@ -16,7 +16,7 @@ const app = express();
 //These two are parsers
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
-app.use(express.static(path.join(_dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs')
 
 
