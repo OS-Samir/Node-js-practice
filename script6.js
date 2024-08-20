3b947a1af09cda7f2d4595da453e386a991554af
OS-Samir <samiradhikary972@gmail.comr>
2024-08-20 13:49:37 +0545

    middle ware added

--- script6.js
+++ script6.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const app = express();
+const express = require('path');
+
+
+app.set("view engine", "ejs");
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")))
 
 app.get('/', function (req, res) {
     res.send("welcome");
