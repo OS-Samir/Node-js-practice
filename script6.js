22981957de9bf1c1434f377cbaa58922c47a01e4
OS-Samir <samiradhikary972@gmail.comr>
2024-08-20 17:16:43 +0545

    day 6 completed

--- script6.js
+++ script6.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require("path");
+const fs = require("fs");
 
 
 app.set("view engine", "ejs");
@@ -9,8 +10,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")))
 
 app.get('/', function (req, res) {
+    fs.readdir(`./files`, function (err, files) {
+        res.render('index', {files: files});
+    })
     // res.send("welcome");
-    res.render("index")
+   
 })
+
+app.post('/create', function (req, res) {
+        fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details, function (err){
+            res.redirect('/')
+        })
+    })
+    // res.send("welcome");
+   
+
 
 app.listen(3000);
