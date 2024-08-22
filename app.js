ed6c08e2b3aa3942d33fefd614c53df5b1ab09f8
OS-Samir <samiradhikary972@gmail.comr>
2024-08-22 22:28:14 +0545

    read users

--- app.js
+++ app.js
@@ -15,9 +15,9 @@ app.get('/', (req,res) => {
 
 app.get('/create', async (req,res) => {
    let createdUser = await userModel.create({
-        name: "samir",
-        email: "samir@gmail.com",
-        username: "samir"
+        name: "suraj karki",
+        email: "suraj@gmail.com",
+        username: "suraj"
     })
     res.send(createdUser);
 })
@@ -30,5 +30,11 @@ app.get('/update', async (req,res) => {
      res.send(updatedUser);
  })
  
+ app.get('/read', async  (req,res) =>  {
+
+ let users = await userModel.findOne({username: "suraj" }); //find() read and search all users //find gives object while fineone gives array
+ res.send(users);
+})
+
 
 app.listen(3000)
