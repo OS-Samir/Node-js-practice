bb6b2712a0183040d1c92d6a64d50cc885ff40bc
OS-Samir <samiradhikary972@gmail.comr>
2024-08-22 18:29:50 +0545

    learning day 8

--- script6.js
+++ script6.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require("path");
 const fs = require("fs");
-const { log } = require('console');
+
 
 
 app.set("view engine", "ejs");
