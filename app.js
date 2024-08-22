53fb3067cedcd58cb92206dbc0f1119f5b0ecd82
OS-Samir <samiradhikary972@gmail.comr>
2024-08-22 18:53:56 +0545

    userschema created

--- app.js
+++ app.js
@@ -2,3 +2,13 @@
 // Types of DBS
 // What and why
 // terminologies - collections, documents, schemas, keys, models
+
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req,res) => {
+    res.send("hello")
+})
+
+app.listen(3000)
