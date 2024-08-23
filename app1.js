966ef23756ed12855310ee8db65af0cf60d88e18
OS-Samir <samiradhikary972@gmail.comr>
2024-08-23 22:24:43 +0545

    user model configuration

--- app1.js
+++ app1.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const path = require("path")
+const userModel = require('./models/user')
+
 
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('view engine', 'ejs');
+app.use
 
 app.get("/", (req, res) =>  {
     res.render("index1")
@@ -14,8 +17,18 @@ app.get("/", (req, res) =>  {
 app.get("/read", (req, res) =>  {
     res.render("read")
 })
-app.post("/create", (req, res) =>  {
-    res.render("read")
+app.post("/create", async (req, res) =>  {
+    let {name, username, image} = req.body;
+
+   let createdUser =  await userModel.create(
+        {
+            name,
+            email,
+            image
+        }
+    );
+
+    res.send(createdUser)
 })
 
 app.listen(3000)
