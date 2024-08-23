ff56d12afa02cca88e059bb0a9ce0ea8575f6205
OS-Samir <samiradhikary972@gmail.comr>
2024-08-23 21:26:54 +0545

    file setup

--- app1.js
+++ app1.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const path = require("path")
 
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static(path.join(__dirname, 'public')))
+app.use('view engine', 'ejs');
 
 app.get("/", (req, res) =>  {
-    res.send("hello worlf")
+    res.send("hello world")
 })
 
 app.listen(3000)
