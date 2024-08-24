97740636f96b24a65342488672e1058f82e2f7b7
OS-Samir <samiradhikary972@gmail.comr>
2024-08-24 21:59:05 +0545

    bug fixed

--- app3.js
+++ app3.js
@@ -1,6 +1,9 @@
+const cookieParser = require('cookie-parser');
 const express = require('express');
 const app = express();
-const cookie = require('cookie')
+
+
+app.use(cookieParser())
 
 app.get('/', function(req, res){
     res.cookie("name", "samir")
@@ -9,6 +12,7 @@ app.get('/', function(req, res){
 
 
 app.get('/read', function(req, res){
+    console.table(req.cookies)
     res.send("Read Page")
 })
 
