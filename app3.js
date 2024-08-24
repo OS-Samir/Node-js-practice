bebf0e8c2708de020a14738297ee8ce79fbe657e
OS-Samir <samiradhikary972@gmail.comr>
2024-08-24 22:15:20 +0545

    encrypt password

--- app3.js
+++ app3.js
@@ -1,20 +1,19 @@
-const cookieParser = require('cookie-parser');
+
 const express = require('express');
 const app = express();
+const bcrypt = require('bcrypt');
 
 
-app.use(cookieParser())
 
 app.get('/', function(req, res){
-    res.cookie("name", "samir")
-    res.send("done")
+   bcrypt.genSalt(10, function(err, salt){
+        bcrypt.hash("samir123", salt, function(err, hash){
+            
+           
+        })
+   })   
 })
 
-
-app.get('/read', function(req, res){
-    console.table(req.cookies)
-    res.send("Read Page")
-})
 
 
 app.listen('3000');
