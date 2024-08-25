15f296913da41c438b8c1180985db6bf7a0ef085
OS-Samir <samiradhikary972@gmail.comr>
2024-08-26 00:04:16 +0545

    jwt token revision completed

--- app3.js
+++ app3.js
@@ -11,6 +11,9 @@ const express = require('express')
 const PORT = 3000;
 const app = express()
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser');
+app.use(cookieParser())
 // const cookieParser = require('cookie-parser')
 // app.use(cookieParser())
 
@@ -26,19 +29,33 @@ const bcrypt = require('bcrypt')
 
 // })
 
-app.get("/", function (req, res) {
-    // bcrypt.genSalt(10, function(err, salt) {
-    //     bcrypt.hash("samir123", salt, function(err, hash){  
-    //         console.log(hash);
-    //     })
-    //     // console.log(salt);  
-    // })
+// app.get("/", function (req, res) {
+//     // bcrypt.genSalt(10, function(err, salt) {
+//     //     bcrypt.hash("samir123", salt, function(err, hash){  
+//     //         console.log(hash);
+//     //     })
+//     //     // console.log(salt);  
+//     // })
 
-    bcrypt.compare("samir123", "$2b$10$da6rEumi/5HE9PUl7XoXF.NmQ9AlS1zWISpLdJTasNRH6QLOTLcxa", function (err, result) { // check if hash password and plain password are equal or not
-        console.log(result)
-    })
+//     bcrypt.compare("samir123", "$2b$10$da6rEumi/5HE9PUl7XoXF.NmQ9AlS1zWISpLdJTasNRH6QLOTLcxa", function (err, result) { // check if hash password and plain password are equal or not
+//         console.log(result)
+//     })
+// })
+
+
+app.get("/", function (req, res) {
+let token = jwt.sign({email: "samir123@gmail.com"}, "secret")
+res.cookie("token", token)
+res.send("done")
+console.log(token)
 })
 
+app.get("/read", function(req, res) {
+    res.send("This is read page ")
+    // console.log(req.cookies.token)
+    let data = jwt.verify(req.cookies.token, "secret")
+    console.log(data)
+})
 
 app.listen(PORT)
 console.log(`App is listening on port http://localhost:${PORT}`)
