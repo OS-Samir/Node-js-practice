6341e852b5ad0f220ec563ea307d19ee92c7bd43
OS-Samir <samiradhikary972@gmail.comr>
2024-08-26 13:39:07 +0545

    setting up ejs file

--- app4.js
+++ app4.js
@@ -2,14 +2,17 @@ const express = require('express')
 const app = express()
 const PORT = 3000;
 const path = require('path')
+const cookieParser = require('cookie-parser')
+
 
 app.use(express.json());
 app.use(express.urlencoded ({extended: true}));
 app.set("view engine", "ejs")
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
-    res.send("welcome")
+    res.render("index3")
 })
 
 app.listen(PORT)
