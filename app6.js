26fd20233d931c1cb840771ed5bf059b7c3140c6
OS-Samir <samiradhikary972@gmail.comr>
2024-08-28 23:44:31 +0545

    implementbcrypt

--- app6.js
+++ app6.js
@@ -1,10 +1,29 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const userModel = require("./models/user6");
+const cookieParser = require('cookie-parser')
+const bcrypt = require('bcrypt');
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser())
 
 app.get("/", (req, res) => {
-    res.send("samir ")
+   res.render("index6")
 })
+
+app.post("/register", async (req, res) => {
+    // let {email, password, username, name, age} = req.body;
+    // let user = await userModel.findOne({email});
+    // if(user) return res.status(500).send("User already registered");
+
+    bcrypt.genSalt(10, (err, salt) => {
+        console.log(salt);
+    })
+})
+
 
 app.listen(3000, () => {
     console.log(`App is listening on http://localhost:${PORT}`)
