2b448f68e8c0cf7f3126bd325c28f9ef32865d47
OS-Samir <samiradhikary972@gmail.comr>
2024-08-29 19:01:10 +0545

    middleware implemented

--- app6.js
+++ app6.js
@@ -18,9 +18,26 @@ app.get("/", (req, res) => {
    res.render("index6")
 })
 
+app.get("/login", (req, res) => {
+    res.render("login6")
+ })
+ 
+ app.get("/logout", (req, res) => {
+    res.cookie("token", "")
+    res.redirect("/login")
+ })
+
+ function isLoggedIn(req, res, next) {
+    if(req.cookies.token === "") res.send("You must be logged in")
+    else {
+       let data = jwt.verify(req.cookies.token, "shhh")
+        }
+    next();
+ }
+ 
 
 app.post("/register", async (req, res) => {
-    let {email, password, username, name, age} = req.body;
+    let {email, password, username, age, name} = req.body;
     let user = await userModel.findOne({email});
     if (user) return res.status(500).send("User already registered")
 
@@ -41,6 +58,18 @@ app.post("/register", async (req, res) => {
     })
 })
 
+
+
+app.post("/login", async (req, res) => {
+    let {email, password} = req.body;
+    let user = await userModel.findOne({email});
+    if (!user) return res.status(400).send("something went wrong")
+
+    bcrypt.compare(password, user.password, function (err, result){
+        if (result) return res.status(200).send("login successful")
+        else res.redirect("./login")
+    })
+})
 
 app.listen(PORT, () => {
 console.log(`App is listening on http://localhost:${PORT}`)
