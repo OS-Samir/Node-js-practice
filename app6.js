a3dc06b32b0beae9ea9d9bd98385a4719c1401c8
OS-Samir <samiradhikary972@gmail.comr>
2024-09-04 23:29:15 +0545

    multer implemented

--- app6.js
+++ app6.js
@@ -6,21 +6,44 @@ const cookieParser = require('cookie-parser')
 const postModel = require('./models/post6')
 const bcrypt = require('bcrypt');
 const jwt = require("jsonwebtoken");
-
+const crypto = require('crypto');
+const path = require('path');
 
 
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(cookieParser())
+app.use(cookieParser());
+
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './public/images/uploads')
+    },
+    
+    filename: function (req, file, cb) { 
+    crypto.randomBytes(12, function  (err, bytes) {
+        const fn = bytes.toString("hex") + path.extname()
+        cb(null, file.fieldname + '-' + uniqueSuffix)
+    })
+  }
+  })
+  
+  const upload = multer({ storage: storage })
 
 app.get("/", (req, res) => {
    res.render("index6")
 })
 
 app.get("/test", (req, res) => {
-    res.render("test")
+    res.render("test6")
  })
+
+
+ app.post("/upload", (req, res) => {
+    console.log(req.body)
+ })
+
 
 app.get("/login", (req, res) => {
     res.render("login6")
